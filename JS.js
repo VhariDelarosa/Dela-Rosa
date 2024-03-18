beb8a6e21dc95fdcd15df8b0d4db8a3ec92176de
VhariDelarosa <163824108+VhariDelarosa@users.noreply.github.com>
2024-03-18 23:39:45 +0800

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,14 @@
+const audio = document.querySelector("audio");
+
+// Example: Add custom play/pause buttons
+const playButton = document.getElementById("play-button");
+const pauseButton = document.getElementById("pause-button");
+
+playButton.addEventListener("click", () => {
+    audio.play();
+});
+
+pauseButton.addEventListener("click", () => {
+    audio.pause();
+});
+Write to Delinquents, Mentally Challenged, Jesus, And Mentally Unstable
